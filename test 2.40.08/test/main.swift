07b817570b811b640576dd5e27de7dfbd6d73fe0
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2015-10-21 03:08:28 +0900

    Update main.swift

--- test 2.40.08/test/main.swift
+++ test 2.40.08/test/main.swift
@@ -2,8 +2,8 @@
 //  main.swift
 //  test
 //
-//  Created by KKZoo on 2015/10/21.
-//  Copyright © 2015年 Motoki. All rights reserved.
+//  Created by Motoki on 2015/10/21.
+//  Copyright © 2015年 Motoki All rights reserved.
 //
 
 import Foundation
